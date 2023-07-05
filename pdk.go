7e6d291c67d2b4c825b258763b91d91364980c46
Phakorn Kiong <phakorn214@gmail.com>
2023-07-05 18:35:05 +0400

    test

--- pdk.go
+++ pdk.go
@@ -1,9 +1,43 @@
 package pdk
 
 import (
+	"encoding/json"
+
 	"github.com/extism/go-pdk"
 )
 
-func AllocateString(data string) pdk.Memory {
-	return pdk.AllocateBytes([]byte(data))
+func Input(v interface{}) error {
+	input := pdk.Input()
+	return json.Unmarshal(input, &v)
+}
+
+func MemoryToBytes(mem pdk.Memory) []byte {
+	buf := make([]byte, mem.Length())
+	mem.Load(buf)
+	return buf
+}
+
+func MemoryToStruct(mem pdk.Memory, v interface{}) error {
+	bytes := MemoryToBytes(mem)
+	return json.Unmarshal(bytes, &v)
+}
+
+func BytesToMemory(b []byte) (mem pdk.Memory) {
+	mem = pdk.AllocateBytes(b)
+	return
+}
+
+func StructToMemory(v interface{}) (mem pdk.Memory, err error) {
+	b, err := json.Marshal(&v)
+
+	if err != nil {
+		return
+	}
+
+	mem = BytesToMemory(b)
+	return
+}
+
+func GetConfig(key string) (string, bool) {
+	return pdk.GetConfig(key)
 }
