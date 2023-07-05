900a6e77fe8c12ee2f43d5a85671dd7666a4791a
Phakorn Kiong <phakorn214@gmail.com>
2023-07-05 18:55:28 +0400

    test2

--- pdk.go
+++ pdk.go
@@ -6,28 +6,35 @@ import (
 	"github.com/extism/go-pdk"
 )
 
+type Memory struct {
+	pdk.Memory
+}
+
 func Input(v interface{}) error {
 	input := pdk.Input()
 	return json.Unmarshal(input, &v)
 }
 
-func MemoryToBytes(mem pdk.Memory) []byte {
+func MemoryToBytes(mem Memory) []byte {
 	buf := make([]byte, mem.Length())
 	mem.Load(buf)
 	return buf
 }
 
-func MemoryToStruct(mem pdk.Memory, v interface{}) error {
+func MemoryToStruct(mem Memory, v interface{}) error {
 	bytes := MemoryToBytes(mem)
 	return json.Unmarshal(bytes, &v)
 }
 
-func BytesToMemory(b []byte) (mem pdk.Memory) {
-	mem = pdk.AllocateBytes(b)
-	return
+func BytesToMemory(b []byte) Memory {
+	return Memory{pdk.AllocateBytes(b)}
 }
 
-func StructToMemory(v interface{}) (mem pdk.Memory, err error) {
+func FindMemory(offset uint64) Memory {
+	return Memory{pdk.FindMemory(offset)}
+}
+
+func StructToMemory(v interface{}) (mem Memory, err error) {
 	b, err := json.Marshal(&v)
 
 	if err != nil {
